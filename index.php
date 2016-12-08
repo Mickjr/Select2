88733c26e08d6383c8f059afd0f8535cee3cd552
Horton Jr., Manton <MHorton@bgea.org>
2016-12-08 14:27:20 -0500

    First Commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+
+  <title>Autocomplete textbox using jQuery, PHP and MySQL by CodexWorld</title>
+
+  <!--  Link stylesheet -->
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  <link rel="stylesheet" href="/vendors/select2/css/select2.css">
+  <!--  Link jQuery -->
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+  <script src="/vendors/select2/js/select2.js"></script>
+
+  <script>
+  $(function() {
+   
+    $( "#skills" ).select2();
+
+  });
+  
+  </script>
+
+</head>
+<body>
+ 
+<div class="ui-widget">
+
+  <select name="skills" id="skills">
+      <option value="1">Writer</option>
+      <option value="2">Editor</option>
+      <option value="3">Subscriber</option>
+      <option value="3">Yekcim</option>
+      <option value="3">Notroh</option>
+      <option value="3">Global</option>
+      <option value="3">Buyer</option>
+  </select>
+
+
+
+</div>
+
+
+<script type="text/javascript">
+  
+
+</script>
+
+
+</body>
+</html>
